d0231a6a185aa58ca0d4cbe5555de51b2301d48b
Michael Harrison <94325409+MichaelHarris0n@users.noreply.github.com>
2024-02-29 18:59:00 +0000

    Node descriptions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'reactflow/dist/style.css';
 import { useCallback, useState, useEffect, useRef } from 'react';
 import ReactFlow, { useNodesState, useEdgesState, addEdge, applyNodeChanges, applyEdgeChanges, MarkerType, ReactFlowProvider, Background } from 'reactflow';
+import { SmartBezierEdge, SmartStraightEdge, SmartStepEdge } from '@tisoap/react-flow-smart-edge'
 import NodeType from "./CircleNode";
 import ContextMenu from './ContextMenu'
 
@@ -9,12 +10,17 @@ const nodeTypes = {
   circleNode: NodeType
 };
 
+const edgeTypes = {
+  smart: SmartBezierEdge,
+  smartStraight: SmartStraightEdge
+}
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: 'q0', finalState: false }, type: "circleNode"},
-  { id: '2', position: { x: 0, y: 0 }, data: { label: 'q1', finalState: false }, type: "circleNode" }
+  { id: '1', position: { x: 0, y: 0 }, data: { label: 'q0', finalState: false, description: '' }, type: "circleNode"},
+  { id: '2', position: { x: 0, y: 0 }, data: { label: 'q1', finalState: false, description: '' }, type: "circleNode" }
 ];
 const initialEdges = [
-  { id: 'e1-3', source: '1', target: '2', label: 'choose', markerEnd: {type: MarkerType.ArrowClosed, width: 20, height: 20}, style:  {strokeWidth: 2} },
+  //{ id: 'e1-3', source: '1', target: '2', label: '1', markerEnd: {type: MarkerType.ArrowClosed, width: 20, height: 20}, style:  {strokeWidth: 2}, type: "smart"},
 ];
 
 
@@ -24,9 +30,11 @@ const App = () => {
   
   const [edgeName, setEdgeName] = useState("Edge 1");
   const [inputString, setInputString] = useState('');
-  const [inputList, setInputList] = useState([]);
+  const [inputs, setInputs] = useState([]);
+  const [nodeDescription, setNodeDescription] = useState('');
 
   const [selectedEdge, setSelectedEdge] = useState(null);
+  const [selectedNode, setSelectedNode] = useState(null);
   const [isSelected, setIsSelected] = useState(false);
 
   const [currentState, setCurrentState] = useState(nodes.find(node => node.id === '1'));
@@ -42,6 +50,7 @@ const App = () => {
       id: (edges.length + 1).toString(),
       markerEnd: {type: MarkerType.ArrowClosed, width: 20, height: 20}, 
       style:  {strokeWidth: 2},
+      type: params.source == params.target? "smart" : ""
     };
 
     setEdges((prevElements) => [...prevElements, newEdge]);
@@ -85,12 +94,25 @@ const App = () => {
       })
     })
   }, [edgeName, setEdges]);
+
   
+  
+  useEffect(() => {
+    setNodes((nds) => {
+      return nds.map((node) => {
+        if (node.id == selectedNode?.id) {
+          node.data.description = nodeDescription;
+        }
+  
+        return node;
+      })
+    })
+  }, [nodeDescription, setNodes])
 
   const addCircleNode = () => {
     const newNode = {
       id: (nodes.length + 1).toString(),
-      data: { label: `q${nodes.length}`, finalState: false },
+      data: { label: `q${nodes.length}`, finalState: false, description: nodeDescription },
       type: "circleNode",
       position: {
         x: 0,
@@ -130,14 +152,16 @@ const App = () => {
 
   useEffect(() => {
     if (isAccepted) {
-      setOutputMessage("Input accepted!");
+      setOutputMessage("Pass");
     } else {
-      setOutputMessage("Input not accepted!");
+      setOutputMessage("Fail");
     }
   }, [isAccepted]);
 
   const onNodeContextMenu = useCallback(
     (event, node) => {
+      setSelectedNode(node);
+
       event.preventDefault();
 
       const pane = ref.current.getBoundingClientRect();
@@ -148,7 +172,9 @@ const App = () => {
         right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
         bottom:
           event.clientY >= pane.height - 200 && pane.height - event.clientY,
+        data: node.data
       });
+      console.log(node.data);
     },
     [setMenu],
   );
@@ -167,11 +193,19 @@ const App = () => {
   const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
 
   return (
-    <>
+    <div style={{backgroundColor:'#EFFAFD'}}>
     <h1 class="text-5xl font-extrabold dark:text-white">Automata Simulator</h1>
     <ReactFlowProvider>
-    <div style={{ display: 'flex' }}>
-      <div style={{ position: 'relative', width: '800px', height: '500px', border: '1px solid black' }}>
+    <div class="grid grid-cols-3 grid-rows-5 gap-1 items-top justify-center" style={{ display: 'flex', border: '1px solid black' }}>
+      <div class="w-1/5" style={{ border: '1px solid black'}}>
+        <label class="font-extrabold dark:text-white">Input Strings: </label>
+        <div className="updatenode_checkboxwrapper flex" style={{ alignContent: 'center', dispaly: 'flex' }}>
+          <input style={{ width: '70%'}} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={inputString} onChange={(evt) => setInputString(evt.target.value)} />
+          <div class="font-extrabold flex align-middle" style={{color: isAccepted? 'green' : 'red'}}>{outputMessage}</div>
+        </div>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={() => checkInputString(inputString)}>Check Inputs</button>
+      </div>
+      <div class="w-3/5 row-start-1" style={{ position: 'relative', width: '800px', height: '500px', border: '1px solid black' }}>
         <ReactFlow 
           ref={ref}
           nodes={nodes}
@@ -180,13 +214,25 @@ const App = () => {
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
           nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
           onNodeContextMenu={onNodeContextMenu}
         >
           <Background />
         {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
         </ReactFlow>
       </div>
-      <div className="updatenode_checkboxwrapper" style={{ display: 'absolute', alignItems: 'top' }}>
+     
+      <div class="w-1/5 font-extrabold dark:text-white align-top row-start-1" style={{ border: '1px solid black'}}>
+        {selectedNode? (
+          <div class="" style={{ border: '1px solid black'}}>
+            <div style={{ border: '1px solid black'}}>State: {selectedNode.data.label}</div>
+            <div style={{ border: '1px solid black'}}>Description:
+              <textarea class='outline-none' style={{width : '100%', backgroundColor:'#EFFAFD'}} value={selectedNode.data.description} onChange={(evt) => setNodeDescription(evt.target.value)}></textarea>
+            </div>
+
+          </div>
+        ): null}
+
       </div>
     </div>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={() => addCircleNode()}>Add Node</button>
@@ -194,14 +240,8 @@ const App = () => {
       <label class="font-extrabold dark:text-white">Label: </label>
       <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={edgeName} onChange={(evt) => setEdgeName(evt.target.value)} />
     </div>
-    <div className="updatenode_checkboxwrapper" style={{ display: 'flex', alignItems: 'center' }}>
-      <label class="font-extrabold dark:text-white">Input String: </label>
-      <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={inputString} onChange={(evt) => setInputString(evt.target.value)} />
-    </div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={() => checkInputString(inputString)}>Check Input</button>
-    <div>{outputMessage}</div>
     </ReactFlowProvider>
-    </>
+    </div>
   )
 }
 
--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -7,18 +7,10 @@ export default function ContextMenu({
   left,
   right,
   bottom,
+  data,
   ...props
 }) {
-  const { getNode, setNodes, addNodes, setEdges } = useReactFlow();
-  const duplicateNode = useCallback(() => {
-    const node = getNode(id);
-    const position = {
-      x: node.position.x + 50,
-      y: node.position.y + 50,
-    };
-
-    addNodes({ ...node, id: `${node.id}-copy`, position });
-  }, [id, getNode, addNodes]);
+  const { setNodes, setEdges } = useReactFlow();
 
   const deleteNode = useCallback(() => {
     setNodes((nodes) => nodes.filter((node) => node.id !== id));
@@ -37,6 +29,9 @@ export default function ContextMenu({
 
   }, [id, setNodes])
 
+  console.log("Hi");
+  console.log(data);
+
   return (
     <div
       style={{ top, left, right, bottom, position: 'absolute' }}
@@ -44,7 +39,7 @@ export default function ContextMenu({
       {...props}
     >
       <p class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" style={{ margin: '0.5em' }}>
-        node: {id}
+        State: {data.label}
       </p>
       <button class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={deleteNode}>delete</button>
       <button class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={setfinalState}>Toggle Final State</button>
